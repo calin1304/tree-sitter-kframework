dd3c4863111e207597143fedcc90dd3dc293ea81
Calin Nicolau <calin.nicolau@gmail.com>
2021-08-26 21:52:29 +0300

    Parsing inner syntax is so difficult :(

--- grammar.js
+++ grammar.js
@@ -1,21 +1,30 @@
 module.exports = grammar({
     name: "kframework",
-
+    extras: $ => [
+        /\s/,
+        $.comment
+    ],
+    inline: $ => [
+        $.upper_id,
+        $.lower_id,
+        $.attribute_name,
+        $.builtin_sort
+    ],
     rules: {
-        source_file: $ => seq(
+        source_expression: $ => seq(
             repeat($.require),
-            repeat(choice($.comment, $.module))
+            repeat($.module)
         ),
 
         require: $ => seq('require', $.require_file),
-        require_file: $ => /"[A-Za-z0-9.]+"/,
+        require_file: $ => /"[A-Za-z0-9./]+"/,
 
         // Module definition {{{
         module: $ => seq(
             'module',
             $.module_name,
             repeat($.imports),
-            repeat($.sentence),
+            repeat($._sentence),
             'endmodule'
         ),
         // }}}
@@ -25,13 +34,12 @@ module.exports = grammar({
         module_name: $ => /#?(\w|-)+/,
         /// }}}
 
-        sentence: $ => choice(
+        _sentence: $ => choice(
             $.claim,
             $.context,
             $.configuration,
             $.rule,
             $.syntax,
-            $.comment
         ),
 
         claim: $ => "claim",
@@ -45,38 +53,47 @@ module.exports = grammar({
         cell_end: $ => seq('<', /[a-zA-Z_](\w|-)*/, '>'),
         // }}}
 
+        // Syntax definition {{{
+        syntax: $ => seq('syntax', $.sort_name, "::=", $.syntax_def),
+
+        syntax_def: $ => sep1($.syntax_def_entry, '|'),
+        syntax_def_entry: $ => /[a-zA-Z()"&\s]+\\n/,
+
+        // syntax_def: $ => seq(
+        //     $.syntax_def_token,
+        //     repeat(seq('|', $.syntax_def_token))
+        // ),
+        // syntax_def_token: $ =>  /"[a-z]+"/,
+
+        // syntax_def: $ => seq(
+        //     $.syntax_def_entry,
+        //     repeat(seq(choice('|', '>'), $.syntax_def_entry))
+        // ),
+        // syntax_def_entry: $ => seq(
+        //     $.syntax_def_tokens,
+        //     $.attribute_list,
+        // ),
+        // syntax_def_tokens: $ => choice(
+        //     repeat1(choice($.sort_name, $.quoted))
+
+        // ),
+        // syntax_token: $ => /"#?\w+"/,
+        // syntax_constructor: $ => /.+/, // seq($.identifier, '(', $._constructor_args, ')'),
+        // // _constructor_args: $ => seq($.sort_name, repeat(seq(',', $.sort_name))),
+
+        // attribute_list: $ => seq(
+        //     '[',
+        //     seq($.attribute_name, repeat(seq(',', $.attribute_name))),
+        //     ']'
+        // ),
+        // attribute_name: $ => /[\w()]+/,
+        // }}}
+
         // Rule definition {{{
         rule: $ => seq('rule', $._rule_lhs, '=>', $._rule_rhs),
 
         _rule_lhs: $ => /.*/,
         _rule_rhs: $ => /.+/,
-        // }}}
-
-        // Syntax definition {{{
-        syntax: $ => seq('syntax', $.sort_name, "::=", $.syntax_def),
-
-        syntax_def: $ => seq(
-            $.syntax_def_entry,
-            repeat(seq(choice('|', '>'), $.syntax_def_entry))
-        ),
-        syntax_def_entry: $ => seq(
-            $.syntax_def_tokens,
-            $.attribute_list,
-        ),
-        syntax_def_tokens: $ => choice(
-            repeat1(choice($.sort_name, $.quoted))
-
-        ),
-        syntax_token: $ => /"#?\w+"/,
-        syntax_constructor: $ => /.+/, // seq($.identifier, '(', $._constructor_args, ')'),
-        // _constructor_args: $ => seq($.sort_name, repeat(seq(',', $.sort_name))),
-
-        attribute_list: $ => seq(
-            '[',
-            seq($.attribute_name, repeat(seq(',', $.attribute_name))),
-            ']'
-        ),
-        attribute_name: $ => /[\w()]+/,
         // }}}
 
         // Misc {{{
@@ -85,12 +102,14 @@ module.exports = grammar({
         lower_id: $ => /#?[A-Z][a-zA-Z0-9]*/,
         sort_name: $ => choice($.builtin_sort, $.upper_id),
         builtin_sort: $ => choice("Bool", "String", "Int"),
-        comment: $ => seq('//', /.*/),
-        quoted: $ => /".*"/,
+        comment: $ => token(seq('//', /.*/)),
         // }}}
-    },
-
-    inline: $ => [
-        $.upper_id, $.lower_id, $.attribute_name, $.quoted, $.builtin_sort
-    ]
+    }
 })
+
+function sep1(rule, separator) {
+    return seq(rule, repeat(seq(separator, rule)));
+}
+function between(rule, c) {
+    return seq(c, rule, c);
+}
